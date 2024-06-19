75aab2a40e2335d831fb87ab048f1b96d965fab6
Giovanni <Gio.orellanac@duocuc.cl>
2024-06-19 08:55:42 -0400

    Inicio de funciones

---
+++ principal.py
@@ -0,0 +1,16 @@
+from funciones import *
+
+while True:
+    print("MENÚ CONTACTOS")
+    print("1. agregar contacto\n2.mostrar contactos\n3.guardar archivo CSV\n4.salir")
+    opc=int(input("Ingrese opción: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    elif opc==4:
+        pass
+    else:
+        pass
